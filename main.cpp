b3801753b00854514cac168357ee2bc5e0d450e3
kbuffardi <kbuffardi@ecc-jaguar.csuchico.edu>
2018-10-14 14:25:43 -0700

    Starter code

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "Bitmap/Bitmap.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+//argc - number of arguments in command
+//argv - each argument word, stored in an array
+int main(int argc, char* argv[])
+{
+  
+  return 0;
+}
