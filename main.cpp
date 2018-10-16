df2182bb7a0b2390ab8de31cf012dc429051395f
xar8 <jevans42@ecc-jaguar.csuchico.edu>
2018-10-15 17:30:48 -0700

    First Draft

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "Bitmap/bitmap.h"
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
@@ -8,6 +9,26 @@ using namespace std;
 //argv - each argument word, stored in an array
 int main(int argc, char* argv[])
 {
-  
+  string filename;
+  Bitmap image;
+
+  if(argc>2 || argc<2)
+    {
+     cout<<"Please specify one image file!\n";
+    }
+  else
+    {
+      filename = argv[1];
+      image.open(filename);
+      if(image.isImage())
+       {
+         vector <vector <Pixel> > imagePixels = image.toPixelMatrix();        
+         cout<<filename << " is " << imagePixels.size() << " high and " << imagePixels[0].size() << " pixels wide" << endl;
+       }
+      else
+       {
+         cout<<"Image file must be a bitmap with 24-bit color depth.\n";
+       }
+    }
   return 0;
 }
